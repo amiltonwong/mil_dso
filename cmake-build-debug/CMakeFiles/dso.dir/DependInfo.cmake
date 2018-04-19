03d6c30752d2436602481e037c9e60af410f5822
amiltonwong <amiltonwong@gmail.com>
2018-04-19 06:45:49 -0400

    commit

---
+++ cmake-build-debug/CMakeFiles/dso.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/code/mil_dso/src/FullSystem/CoarseInitializer.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/CoarseInitializer.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/CoarseTracker.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/CoarseTracker.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/FullSystem.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystem.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/FullSystemDebugStuff.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystemDebugStuff.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/FullSystemMarginalize.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystemMarginalize.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/FullSystemOptPoint.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystemOptPoint.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/FullSystemOptimize.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/FullSystemOptimize.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/HessianBlocks.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/HessianBlocks.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/ImmaturePoint.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/ImmaturePoint.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/PixelSelector2.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/PixelSelector2.cpp.o"
+  "/data/code/mil_dso/src/FullSystem/Residuals.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/FullSystem/Residuals.cpp.o"
+  "/data/code/mil_dso/src/IOWrapper/OpenCV/ImageDisplay_OpenCV.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/IOWrapper/OpenCV/ImageDisplay_OpenCV.cpp.o"
+  "/data/code/mil_dso/src/IOWrapper/OpenCV/ImageRW_OpenCV.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/IOWrapper/OpenCV/ImageRW_OpenCV.cpp.o"
+  "/data/code/mil_dso/src/IOWrapper/Pangolin/KeyFrameDisplay.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/IOWrapper/Pangolin/KeyFrameDisplay.cpp.o"
+  "/data/code/mil_dso/src/IOWrapper/Pangolin/PangolinDSOViewer.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/IOWrapper/Pangolin/PangolinDSOViewer.cpp.o"
+  "/data/code/mil_dso/src/OptimizationBackend/AccumulatedSCHessian.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/OptimizationBackend/AccumulatedSCHessian.cpp.o"
+  "/data/code/mil_dso/src/OptimizationBackend/AccumulatedTopHessian.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/OptimizationBackend/AccumulatedTopHessian.cpp.o"
+  "/data/code/mil_dso/src/OptimizationBackend/EnergyFunctional.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/OptimizationBackend/EnergyFunctional.cpp.o"
+  "/data/code/mil_dso/src/OptimizationBackend/EnergyFunctionalStructs.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/OptimizationBackend/EnergyFunctionalStructs.cpp.o"
+  "/data/code/mil_dso/src/util/Undistort.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/util/Undistort.cpp.o"
+  "/data/code/mil_dso/src/util/globalCalib.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/util/globalCalib.cpp.o"
+  "/data/code/mil_dso/src/util/settings.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/src/util/settings.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_SSE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../thirdparty/Sophus"
+  "../thirdparty/sse2neon"
+  "/usr/include/eigen3"
+  "/root/Downloads2/ElasticFusion/deps/Pangolin/include"
+  "/root/Downloads2/ElasticFusion/deps/Pangolin/build/src/include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "/usr/include/suitesparse"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/dso_dataset.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/code/mil_dso/src/main_dso_pangolin.cpp" "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso_dataset.dir/src/main_dso_pangolin.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_SSE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../thirdparty/Sophus"
+  "../thirdparty/sse2neon"
+  "/usr/include/eigen3"
+  "/root/Downloads2/ElasticFusion/deps/Pangolin/include"
+  "/root/Downloads2/ElasticFusion/deps/Pangolin/build/src/include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "/usr/include/suitesparse"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/data/code/mil_dso/cmake-build-debug/CMakeFiles/dso.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
